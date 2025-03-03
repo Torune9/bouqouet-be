1fb5f5c0d778ab734cff52598cbbe62f4d095ed5
rahman <rahmannurhayadi@gmail.com>
2025-03-03 10:02:10 +0700

    feat(service) : fix validation data fo profile

--- src/services/middleware/validator.js
+++ src/services/middleware/validator.js
@@ -1,50 +1,54 @@
-const { body,validationResult } = require("express-validator");
-
+const { body, validationResult } = require("express-validator");
 
 // validasi data login
-const validateLogin = [
-    body("email").notEmpty().isEmail().trim().withMessage("Format email tidak valid"),
-    body("password").notEmpty().trim().withMessage("Password wajib di isi"),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        next();
+const validated = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
+    next();
+}
+
+const validateLogin = [
+    body("email")
+        .notEmpty()
+        .isEmail()
+        .trim()
+        .withMessage("Format email tidak valid"),
+    body("password").notEmpty().trim().withMessage("Password wajib di isi"),
+    validated
 ];
 
 // validasi data register
 const validateRegister = [
     body("username").notEmpty().trim().withMessage("username wajib di isi"),
-    body("email").notEmpty().isEmail().trim().withMessage("format email tidak valid"),
-    body("password").notEmpty().isLength({min:8}).trim().withMessage("password minimal 8 karakter"),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        next();
-    }
+    ...validateLogin
+];
+
+//validasi data profile
+const validateProfile = [
+    body(["firstName", "lastName"]).notEmpty().trim().escape(),
+    validated
 ];
 
 // alidasi data produk
 const validateBouqouet = [
-    body("name").notEmpty().escape().trim().withMessage("nama tidak boleh kosong"),
+    body("name")
+        .notEmpty()
+        .escape()
+        .trim()
+        .withMessage("nama tidak boleh kosong"),
     body("price").notEmpty().escape().isInt().withMessage("price harus angka"),
     body("stock").notEmpty().escape().isInt().withMessage("stock harus angka"),
     body("categoryId").optional().escape(),
     body("description").optional().escape().trim(),
     body("image").optional(),
-
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        next();
-    }
+    validated
 ];
 
-
-module.exports = {validateLogin,validateRegister,validateBouqouet}
+module.exports = {
+    validateLogin,
+    validateRegister,
+    validateProfile,
+    validateBouqouet,
+};
