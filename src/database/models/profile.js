4ff1665aafb12090ce36d0c3fc4f0e41f809b752
rahman <rahmannurhayadi@gmail.com>
2025-03-04 10:11:39 +0700

    fix(model) : fix datatype for userId

--- src/database/models/profile.js
+++ src/database/models/profile.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Profile.init({
+    userId : DataTypes.UUID,
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     image: DataTypes.STRING
