d21bc8e0121148c5e027a40f664861006992e022
rahman <rahmannurhayadi@gmail.com>
2025-03-04 12:22:00 +0700

    feat : add delete and detail bouqet endpoint

--- src/router/routes/bouqouetRouter.js
+++ src/router/routes/bouqouetRouter.js
@@ -2,18 +2,25 @@ const express = require("express");
 
 
 const {validateBouqouet} = require("../../services/middleware/validator");
+const { uploadImageProduct } = require("../../services/middleware/uploadFile");
+
 const bouqouetRouter = express.Router();
 
 const addBouqouet = require("../../controller/product/addBouquet");
 const getAllBouqouet = require("../../controller/product/getAllBouqouet");
 const updateBouqouet = require("../../controller/product/updateBouqouet");
-const { uploadImageProduct } = require("../../services/middleware/uploadFile");
+const deleteBouqouet = require("../../controller/product/deleteBouqouet");
+const getDetailBouqouet = require("../../controller/product/detailBouqouet");
 
 
 bouqouetRouter.get("/",getAllBouqouet);
 
+bouqouetRouter.get("/:id",getDetailBouqouet);
+
 bouqouetRouter.post("/",uploadImageProduct,validateBouqouet,addBouqouet);
 
 bouqouetRouter.put("/:id",uploadImageProduct,updateBouqouet);
+
+bouqouetRouter.delete("/:id",deleteBouqouet);
 
 module.exports = bouqouetRouter;
