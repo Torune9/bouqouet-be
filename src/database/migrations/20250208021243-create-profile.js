07351c646cc28e21c9779e2b1a193091ca478e8a
rahman <rahmannurhayadi@gmail.com>
2025-03-04 10:10:13 +0700

    fix(migration) : fix colum name  and constraint userId

--- src/database/migrations/20250208021243-create-profile.js
+++ src/database/migrations/20250208021243-create-profile.js
@@ -1,34 +1,40 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Profiles', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue : Sequelize.literal('gen_random_uuid()')
-      },
-      firstName: {
-        type: Sequelize.STRING(100)
-      },
-      lastName: {
-        type: Sequelize.STRING(100)
-      },
-      image: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Profiles');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("Profiles", {
+            id: {
+                allowNull: false,
+                primaryKey : true,
+                type: Sequelize.UUID,
+                defaultValue : Sequelize.literal('gen_random_uuid()')
+            },
+            userId: {
+                allowNull: true,
+                unique : true,
+                type: Sequelize.UUID,
+            },
+            firstName: {
+                type: Sequelize.STRING(100),
+            },
+            lastName: {
+                type: Sequelize.STRING(100),
+            },
+            image: {
+                type: Sequelize.STRING,
+                allowNull : true
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("Profiles");
+    },
 };
