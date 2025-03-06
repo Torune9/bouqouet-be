098f73e9524dc43adfdd4ac350bd3e45364bc500
rahman <rahmannurhayadi@gmail.com>
2025-03-06 09:24:07 +0700

    feat  : add address eouter

--- src/router/main.js
+++ src/router/main.js
@@ -3,10 +3,12 @@ const express = require('express')
 const userRouter = require('../router/routes/userRouter.js')
 const bouqouetRouter = require('./routes/bouqouetRouter.js')
 const categoryRouter = require('./routes/categoryRouter.js')
+const addressRouter = require('./routes/addressRouter.js')
 
 const mainRouter = express.Router()
 
 mainRouter.use('/user', userRouter)
+mainRouter.use('/user/address', addressRouter)
 mainRouter.use('/category',categoryRouter)
 mainRouter.use('/bouqouet', bouqouetRouter)
 
---
+++ src/router/routes/addressRouter.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const createAddress = require('../../controller/address/createAddress')
+const getAddress = require('../../controller/address/getAddress')
+const updateAddress = require('../../controller/address/updateAddress')
+const deleteAddress = require('../../controller/address/deleteAddress')
+
+const addressRouter = express.Router()
+
+addressRouter.get('/',getAddress)
+
+addressRouter.post('/',createAddress)
+
+addressRouter.put('/:id',updateAddress)
+
+addressRouter.delete('/:id',deleteAddress)
+
+
+module.exports = addressRouter
