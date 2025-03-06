1bbe0f502e8b9e7d224d4fa393f6da97f4ce45ce
rahman <rahmannurhayadi@gmail.com>
2025-03-06 11:13:42 +0700

    fix(router) : add validateAddress middleware

--- src/router/routes/addressRouter.js
+++ src/router/routes/addressRouter.js
@@ -3,12 +3,13 @@ const createAddress = require('../../controller/address/createAddress')
 const getAddress = require('../../controller/address/getAddress')
 const updateAddress = require('../../controller/address/updateAddress')
 const deleteAddress = require('../../controller/address/deleteAddress')
+const { validateAddress } = require('../../services/middleware/validator')
 
 const addressRouter = express.Router()
 
 addressRouter.get('/:id',getAddress)
 
-addressRouter.post('/',createAddress)
+addressRouter.post('/',validateAddress,createAddress)
 
 addressRouter.put('/:id',updateAddress)
 
