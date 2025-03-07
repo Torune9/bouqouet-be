c989efa14ee25ce34aca77bb41581165a2409abc
rahman <rahmannurhayadi@gmail.com>
2025-03-07 16:50:56 +0700

    fix : fix 404 route

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mainRouter = require('./src/router/main')
-const app = express()
-const port = 3000
-
+const express = require("express");
+const mainRouter = require("./src/router/main");
+const app = express();
+const port = 3000;
 
 const urlEncode = express.urlencoded({
-    extended : true
-})
+    extended: true,
+});
 
-app.use(express.json())
-app.use(urlEncode)
+app.use(express.json());
 
-app.use('/api/v1/',mainRouter)
+app.use(urlEncode);
 
-app.listen(port,()=>{
+app.use("/api/v1/", mainRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
+});
+
+app.listen(port, () => {
     console.log(`server running on port ${port}`);
-})
+});
