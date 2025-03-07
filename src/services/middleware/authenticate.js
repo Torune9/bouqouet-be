245ccfa4c668a2a9e4afe30118ae967b3e0b5851
rahman <rahmannurhayadi@gmail.com>
2025-03-07 16:51:46 +0700

    feat(service) : add authenticae middleware

---
+++ src/services/middleware/authenticate.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+
+const authenticate = (req, res, next) => {
+    try {
+        const authHeader = req.get("Authorization");
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res
+                .status(401)
+                .json({ message: "Not authorized" });
+        }
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+         console.log("wkwkw",decoded);
+         
+            if (err) {
+                return res.status(403).json({ message: "Token invalid" });
+            }
+            next(); 
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Terjadi kesalahan di server", error });
+    }
+};
+
+module.exports = authenticate;
