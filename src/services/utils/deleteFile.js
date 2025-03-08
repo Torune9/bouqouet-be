19ee5f854fc432bbbe8c565bfeecb0eebbe5e6e1
rahman <rahmannurhayadi@gmail.com>
2025-03-08 17:39:01 +0700

    refactor(service) : change logic delete files

--- src/services/utils/deleteFile.js
+++ src/services/utils/deleteFile.js
@@ -2,41 +2,46 @@ const fs = require("fs");
 
 const path = require("path");
 
-const cloudinary = require("./cloudinary");
+const cloudinary = require('cloudinary')
 
 const deleteFiles = async (files, dir = "", cloudinaryIds = []) => {
-    
-    if (!files || files.length === 0) return;
+    if (!files || files.length === 0) {
+        if (cloudinaryIds.length > 0) {
+            const deletePromises = cloudinaryIds.map((id) =>
+                cloudinary.v2.uploader.destroy(id)
+            );
 
-    files.forEach((file) => {
-        let filePath;
-
-        if (file.includes("/") || file.includes("\\")) {
-            filePath = path.resolve(file);
-        } else {
-            filePath = path.join(__dirname, `../../assets/${dir}`, file);
-        }
-
-        fs.unlink(filePath, (err) => {
-            if (err) {
-                console.error(`Gagal menghapus file lokal: ${filePath}`, err);
-            } else {
-                console.log(`Berhasil menghapus file lokal: ${filePath}`);
+            try {
+                await Promise.all(deletePromises);
+                console.log(
+                    "Berhasil menghapus gambar dari Cloudinary:",
+                    cloudinaryIds
+                );
+            } catch (error) {
+                console.error("Gagal menghapus gambar dari Cloudinary:", error);
             }
-        });
-    });
-
-    if (cloudinaryIds.length > 0) {
-        const deletePromises = cloudinaryIds.map((id) =>
-            cloudinary.uploader.destroy(id)
-        );
-
-        try {
-            await Promise.all(deletePromises);
-            console.log("Berhasil menghapus gambar dari Cloudinary:", cloudinaryIds);
-        } catch (error) {
-            console.error("Gagal menghapus gambar dari Cloudinary:", error);
         }
+    } else {
+        files.forEach((file) => {
+            let filePath;
+
+            if (file.includes("/") || file.includes("\\")) {
+                filePath = path.resolve(file);
+            } else {
+                filePath = path.join(__dirname, `../../assets/${dir}`, file);
+            }
+
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error(
+                        `Gagal menghapus file lokal: ${filePath}`,
+                        err
+                    );
+                } else {
+                    console.log(`Berhasil menghapus file lokal: ${filePath}`);
+                }
+            });
+        });
     }
 };
 
