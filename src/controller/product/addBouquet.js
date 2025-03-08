1dbc06718f582ec40b06ca5ece619da5cf8b9e93
rahman <rahmannurhayadi@gmail.com>
2025-03-08 12:06:35 +0700

    fix(controller) : fix add image bouqouet

--- src/controller/product/addBouquet.js
+++ src/controller/product/addBouquet.js
@@ -1,47 +1,65 @@
-const { Bouqouet, Category, sequelize } = require("../../database/models");
+const {
+    Bouqouet,
+    Category,
+    ImageBouqouet,
+    sequelize,
+} = require("../../database/models");
 const { matchedData } = require("express-validator");
 
 const deleteFiles = require("../../services/utils/deleteFile");
+const { cloudinary } = require("../../services/utils/cloudinary");
 
 const addBouqouet = async (req, res) => {
     const transaction = await sequelize.transaction();
-    const file = req?.files?.["image"]
-        ? req.files["image"].map((file) => file.filename)
-        : [];
+    const files = req.files["image"]
+        ? req?.files["image"]?.map((file) => file.path)
+        : null;
     try {
         const validated = matchedData(req);
 
         const category = await Category.findByPk(validated["categoryId"]);
 
         if (!category) {
-            deleteFiles(file,"bouqouets");
             return res.status(404).json({
                 message: "Kategori tidak ditemukan",
             });
         }
 
-        const bouqouet = await Bouqouet.create(
-            {
-                ...validated,
-                image: file.length ? file : null,
-            },
-            {
-                transaction,
-            }
-        );
+        const bouqouet = await Bouqouet.create(validated, {
+            transaction,
+        });
 
+        if (files) {
+            const uploadPromises = files.map((file) =>
+                cloudinary.uploader.upload(file, { folder: "bouqouets" })
+            );
+            const uploadResults = await Promise.all(uploadPromises);
+            var imageUrls = uploadResults.map((result) => result.secure_url);
+        }
+        if (imageUrls) {
+            const temp = imageUrls.map((path) => ({
+                bouqouetId: bouqouet.id,
+                path: path,
+            }));
+            var image = await ImageBouqouet.bulkCreate(temp, { transaction });
+        }
+
+        files ? deleteFiles(files, "bouqouets") : "";
         await transaction.commit();
 
         return res.status(200).json({
             message: "Bouquet berhasil dibuat",
             data: bouqouet,
+            image,
         });
     } catch (error) {
         await transaction.rollback();
-        deleteFiles(file);
+        files ? deleteFiles(files, "bouqouets") : "";
+        console.log(error);
+
         return res.status(500).json({
             message: "Error ketika memasukan data bouquet",
-            errors: error.message,
+            errors: error,
         });
     }
 };
