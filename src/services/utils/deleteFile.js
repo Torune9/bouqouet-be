d42b36d1c446a70ebeab62c2803599d40b269003
rahman <rahmannurhayadi@gmail.com>
2025-03-08 12:05:41 +0700

    fix(service) : fix deletefile in cloudinary

--- src/services/utils/deleteFile.js
+++ src/services/utils/deleteFile.js
@@ -1,17 +1,43 @@
 const fs = require("fs");
+
 const path = require("path");
 
-const deleteFiles = (files,dir) => {
+const cloudinary = require("./cloudinary");
+
+const deleteFiles = async (files, dir = "", cloudinaryIds = []) => {
+    
     if (!files || files.length === 0) return;
 
-    files.forEach((filename) => {
-        const filePath = path.join(__dirname, `../../assets/${dir}`, filename);
+    files.forEach((file) => {
+        let filePath;
+
+        if (file.includes("/") || file.includes("\\")) {
+            filePath = path.resolve(file);
+        } else {
+            filePath = path.join(__dirname, `../../assets/${dir}`, file);
+        }
+
         fs.unlink(filePath, (err) => {
             if (err) {
-                console.error(`Gagal menghapus file: ${filePath}`, err);
+                console.error(`Gagal menghapus file lokal: ${filePath}`, err);
+            } else {
+                console.log(`Berhasil menghapus file lokal: ${filePath}`);
             }
         });
     });
+
+    if (cloudinaryIds.length > 0) {
+        const deletePromises = cloudinaryIds.map((id) =>
+            cloudinary.uploader.destroy(id)
+        );
+
+        try {
+            await Promise.all(deletePromises);
+            console.log("Berhasil menghapus gambar dari Cloudinary:", cloudinaryIds);
+        } catch (error) {
+            console.error("Gagal menghapus gambar dari Cloudinary:", error);
+        }
+    }
 };
 
-module.exports =  deleteFiles;
+module.exports = deleteFiles;
