3ec4b979c5ac92c7a6d7e486e9ccacd8a96c2230
rahman <rahmannurhayadi@gmail.com>
2025-03-08 12:06:11 +0700

    fix(model) : fix realtion bouquet

--- src/database/models/bouqouet.js
+++ src/database/models/bouqouet.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Category,Order,OrderItem}) {
+    static associate({Category,Order,OrderItem,ImageBouqouet}) {
       
       this.belongsTo(Category,{
         foreignKey : 'categoryId'
@@ -19,13 +19,16 @@ module.exports = (sequelize, DataTypes) => {
         through : OrderItem,
         foreignKey : 'bouqouetId'
       })
+      this.hasMany(ImageBouqouet,{
+        foreignKey : 'bouqouetId',
+        onDelete : 'CASCADE'
+      })
     }
   }
   Bouqouet.init({
     name: DataTypes.STRING,
     price: DataTypes.BIGINT,
     stock: DataTypes.INTEGER,
-    image: DataTypes.ARRAY(DataTypes.STRING),
     description: DataTypes.TEXT,
     categoryId: DataTypes.UUID
   }, {
