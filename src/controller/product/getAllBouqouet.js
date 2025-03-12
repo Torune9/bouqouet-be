998ea5316848f03b442cb0e40fb477ea2b2bddc2
rahman <rahmannurhayadi@gmail.com>
2025-03-12 08:52:14 +0700

    fix(controller) : adding filterization bouquet

--- src/controller/product/getAllBouqouet.js
+++ src/controller/product/getAllBouqouet.js
@@ -1,10 +1,35 @@
+const { Op } = require('sequelize');
 const {Bouqouet,ImageBouqouet} = require('../../database/models')
 
 const getAllBouqouet = async (req,res)=>{
     try {
+        const whereCondition = {};
+
+        if (req.query.category) {
+            whereCondition.categoryId = req.query.category;
+        }
+
+        if (req.query.stockBelow) {
+            whereCondition.stock = { [Op.lte] : req.query.stockBelow };
+        }
+
+        const sortOptions = {
+            priceAsc: [['price', 'ASC']],
+            priceDesc: [['price', 'DESC']],
+            latest: [['createdAt', 'DESC']],
+            oldest: [['createdAt', 'ASC']],
+            latestPriceAsc: [['createdAt', 'DESC'], ['price', 'ASC']],
+            stock : ['stock','DESC']
+        };
+        
+        const order = sortOptions[req.query.sortBy] || [['createdAt', 'DESC']];
+
         const bouqouets = await Bouqouet.findAll({
-            include : ImageBouqouet
-        })
+            where: whereCondition, 
+            include: [{ model: ImageBouqouet }],
+            order
+        });
+
         return res.status(200).json({
             message : "berhasil mendapatkan list data bouqouet",
             data : bouqouets
