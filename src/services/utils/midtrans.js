2f5caf1dabd25c8249ff4a2bfb29e9746f09a47a
rahman <rahmannurhayadi@gmail.com>
2025-04-03 10:43:41 +0700

    feat(service) : add apiCLient midtrans

--- src/services/utils/midtrans.js
+++ src/services/utils/midtrans.js
@@ -1,9 +1,14 @@
 const midtransClient = require("midtrans-client");
 
-let snap = new midtransClient.Snap({
+const apiClient = new midtransClient.CoreApi({
+    isProduction: false,
+    serverKey: process.env.MIDTRANS_SERVER_KEY,
+    clientKey: process.env.MIDTRANS_CLIENT_KEY,
+});
+
+const snap = new midtransClient.Snap({
     isProduction: false,
     serverKey: process.env.MIDTRANS_SERVER_KEY,
 });
 
-
-module.exports = snap
+module.exports = {apiClient,snap}
