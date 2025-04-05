4237c955bb5e486eff070c54da37211d3356fa54
rahman <rahmannurhayadi@gmail.com>
2025-04-05 17:50:03 +0700

    fix prod env

---
+++ src/database/config/config.js
@@ -0,0 +1,29 @@
+
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "postgres",
+    database: "snfbouquet",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
